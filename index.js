29256fcfbdcb979edc4955bb855b50cd3f5d1128
Ezequiel Alves <junior_ljjg@hotmail.com>
2024-01-12 01:03:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const crypto = require('crypto');
+
+const app = express()
+const port = 3333
+const bd = {
+  email: 'email@email.com',
+  password: '123'
+}
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(express.json())
+app.use(cookieParser())
+
+app.post('/login', (req, res) => {
+  const cookie = req.cookies['1.0.0-cookie']
+  const email = req.body?.email
+  const password = req.body?.password
+
+  const isValidUser = email === bd.email 
+    && password === bd.password
+
+  if (!isValidUser) return res.status(404).send({ statusCode: 404, message: 'User not found'})
+
+  if (cookie === undefined) {
+    const token = crypto.randomUUID()
+    const threeMinutes = 1000 * 60 * 60 * 1 // 1 hour
+
+    res.cookie(
+      '1.0.0-cookie', 
+      token, 
+      { maxAge: threeMinutes, httpOnly: true },
+    )
+
+  }
+
+  res.status(200).send({ statusCode: 200, message: 'User logged'} )
+})
+
+app.get('/server-cookie', (req, res) => {
+  const cookie = req.cookies['1.0.0-cookie']
+
+  if (cookie === undefined) {
+    return res.status(401).send({ statusCode: 400, message: 'User Unauthorized'})
+  }
+
+  res.status(200).send({ statusCode: 200, results: [1, 2, 3]})
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
